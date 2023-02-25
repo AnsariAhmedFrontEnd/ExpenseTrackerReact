8a2f738628380487fa2d8f9aac8da738e3ee9a45
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-02-25 18:39:37 +0530

    Variables added to JS File

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,19 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
+
+    const expenseDate = new Date(2023, 1, 25);
+    const expenseTitle = 'Petrol';
+    const expenseAmount = 200;
+    const locatationOfExpenditure = 'At HP';
     return (
-        <div>
-    <h2>Expense Item!</h2>
-    <h2>Food Rs. 10</h2>
-    <h2>Petrol Rs. 100</h2>
-    <h2>Movies Rs. 200g</h2>
-    </div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle} - {locatationOfExpenditure}</h2>
+                <div className="expense-item__price">Rs. {expenseAmount}</div>
+            </div>
+        </div>
     )
 }
 
