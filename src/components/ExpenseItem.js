0c58635c9d9d163f454559986877401606a1d154
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-02-28 21:30:16 +0530

    Making the component Readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
-import './ExpenseItem.css'
+import ExpenseDeatails from './ExpenseDetails';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-
+  
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title} - {props.locationOfExpenditure}</h2>
-                <div className="expense-item__price">Rs. {props.amount}</div>
-            </div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
+          
         </div>
     )
 }
