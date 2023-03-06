00b45505a79a26ac2c7c7d6409c66cbf3a238dce
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-03-06 09:42:50 +0530

    Understood the Card Element and import React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import ExpenseItem from "./components/ExpenseItem";
+import Card from './components/Card'
 
 function App() {
   const expenses = [
@@ -9,36 +11,53 @@ function App() {
       date: new Date(2020, 7, 14),
       locationOfExpenditure: "Mall",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12), locationOfExpenditure:'Electronic'},
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "Electronic",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      locationOfExpenditure:'Office'
+      locationOfExpenditure: "Office",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      locationOfExpenditure:'Home'
+      locationOfExpenditure: "Home",
     },
   ];
 
   const expenseItem = expenses.map((expense) => {
-   return <ExpenseItem
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-      locationOfExpenditure={expense.locationOfExpenditure}
-    ></ExpenseItem>
-  })
+    return (
+      <ExpenseItem
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        locationOfExpenditure={expense.locationOfExpenditure}
+      ></ExpenseItem>
+    );
+  });
+
+  // return React.createElement(
+  //   Card,
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //    expenseItem
+  // );
+
+  
   return (
-    <div>
+    <Card>
       <h2>Let's get started!</h2>
-      {expenseItem}   
-    </div>
+      {expenseItem}
+    </Card>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
 import ExpenseDeatails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
+import Card from './Card'
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
   
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
           
-        </div>
+        </Card>
     )
 }
 
