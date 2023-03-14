6c742c9e209560944c42bb640dcf6be6984dca50
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-03-14 22:56:19 +0530

    Added price changing button

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './ExpenseItem.css';
 
 const ExpenseDeatails = (props) => {
-    const clickHander = () => {
-        console.log('Clicked!!!')
+    // const clickHander = () => {
+    //     console.log('Clicked!!!')
+    // }
 
-        const deleteExpense = () => {
-            
-        }
-    }
+    // const deleteExpense = () => {
+    //     console.log('deleted')
+    //     }
     return (
         <div className='expense-item__description'>
         <h2>{props.title} - {props.locationOfExpenditure}</h2>
     <div className="expense-item__price">Rs. {props.amount}</div>
-    <button onClick={clickHander}>Change Title</button>
-    <button onClick={deleteExpense}>Delete Item</button>
+    {/* <button onClick={clickHander}>Change Title</button> */}
+    {/* <button onClick={deleteExpense}>Delete Item</button> */}
     </div>
     )
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseDeatails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title)
+    const [price, setPrice] = useState(props.amount);
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title)
+
+    }
+
+    const priceClickHandler = () => {
+            setPrice('100');
+    }
   
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
-          
+            <h2>{title}</h2>
+            <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={price} />
+            <button onClick={priceClickHandler}>Change Price</button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
