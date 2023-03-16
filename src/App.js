8fb4779b67db369b7a8f3e84fda51867b80c6140
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-03-16 18:08:04 +0530

    Rendering Dynamic Data and Adding keys

--- src/App.js
+++ src/App.js
@@ -1,70 +1,52 @@
-import React from "react";
-import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Card from './components/UI/Card';
-import NewExpense from './components/NewExpense/NewExpense'
+import React, {useState} from 'react';
 
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses'
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Mall",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      locationOfExpenditure: "Electronic",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      locationOfExpenditure: "Office",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      locationOfExpenditure: "Home",
-    },
-  ];
 
+const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const expenseItem = expenses.map((expense) => {
-    return (
-      <ExpenseItem
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        locationOfExpenditure={expense.locationOfExpenditure}
-      ></ExpenseItem>
-    );
-  });
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    })
+  };
 
   // return React.createElement(
-  //   Card,
+  //   'div',
   //   {},
-  //   React.createElement("h2", {}, "Let's get started!"),
-  //    expenseItem
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
 
-  const addExpenseHandler = (expense) => {
-    console.log('in App.js');
-    console.log(expenses)
-  }
-  
   return (
-    <Card>
+    <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      {expenseItem}
-    </Card>
+      <Expenses items={expenses} />
+    </div>
   );
 }
 
