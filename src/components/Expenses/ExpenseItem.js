20a0d50be6cd5cf4ec9abf16dcfd95e55764bfa8
AnsariAhmedFrontEnd <am27043@gmail.com>
2023-03-16 16:59:41 +0530

    controlled and uncontrolled components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,30 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ExpenseDeatails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title)
-    const [price, setPrice] = useState(props.amount);
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title)
-
-    }
-
-    const priceClickHandler = () => {
-            setPrice('100');
-    }
-  
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
-            <h2>{title}</h2>
-            <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={price} />
-            <button onClick={priceClickHandler}>Change Price</button>
-            <button onClick={clickHandler}>Change Title</button>
+            <h2>{props.title}</h2>
+            <ExpenseDeatails title={props.title} locationOfExpenditure={props.locationOfExpenditure} amount={props.amount} />
         </Card>
     )
 }
